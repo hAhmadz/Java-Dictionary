ab24653a8bbf0b801368f82914cdc4a5ea67a3b9
hAhmadz <haaris.ahmad1@gmail.com>
2017-09-05 00:47:14 +1000

    Comments Removed

--- Dictionary/src/dictionaryserver/DictionaryServer.java
+++ Dictionary/src/dictionaryserver/DictionaryServer.java
@@ -152,7 +152,6 @@ public class DictionaryServer extends Thread
         String fileName = "Dictionary.txt";
         String key = "";
         String value = "";
-
         try
         {
             FileReader fileReader = new FileReader(fileName);
@@ -182,13 +181,6 @@ public class DictionaryServer extends Thread
         return tempTable;
     }
 
-    
-    
-//    public void InitializeThread(Socket client)
-//    {
-//        myThread t = new myThread("Thread - " + counter , client);
-//        t.start(client);
-//    }
     void sendMessage(String msg)
     {
         try
@@ -223,61 +215,12 @@ public class DictionaryServer extends Thread
         {
             System.out.println(e);
         }
-        //****************
-        //****************Recent Functionality//
-//        try
-//        {
-//            providerSocket = new ServerSocket(port);
-//            System.out.println("Waiting for connection");
-//            connection = providerSocket.accept();
-//            System.out.println("Connection received from " + connection.getInetAddress().getHostName());
-//            out = new ObjectOutputStream(connection.getOutputStream());
-//            out.flush();
-//            in = new ObjectInputStream(connection.getInputStream());
-//            sendMessage("Connection successful");
-//            
-//            do
-//            {
-//                try
-//                {
-//                    message = (String) in.readObject();
-//                    System.out.println("client>" + message);
-//                    if (message.equals("bye"))
-//                    {
-//                        sendMessage("bye");
-//                    }
-//                }
-//                catch (ClassNotFoundException classnot)
-//                {
-//                    System.err.println("Data received in unknown format");
-//                }
-//            }
-//            while (!message.equals("bye"));
-//        }
-//        catch (IOException ioException)
-//        {
-//            ioException.printStackTrace();
-//        }
-//        finally
-//        {
-//            try
-//            {
-//                in.close();
-//                out.close();
-//                providerSocket.close();
-//            }
-//            catch (IOException ioException)
-//            {
-//                ioException.printStackTrace();
-//            }
-//        }
     }
 
     public static void main(String[] args)
     {
         DictionaryServer myServer = new DictionaryServer();
         myServer.Initialize(myServer, args);
-        
         while(true)
             myServer.StartServer(myServer);
     }
--- Dictionary/src/dictionaryserver/myThread.java
+++ Dictionary/src/dictionaryserver/myThread.java
@@ -11,7 +11,6 @@ import java.net.Socket;
 
 public class myThread extends Thread
 {
-
     Socket serverClient;
     int clientNo;
     int squre;
@@ -41,10 +40,6 @@ public class myThread extends Thread
                     outStream.writeUTF("pinging");
                     outStream.flush();
                 }
-//                System.out.println("From Client-" + clientNo + ": Number is :" + clientMessage);
-//                squre = Integer.parseInt(clientMessage) * Integer.parseInt(clientMessage);
-//                serverMessage = "From Server to Client-" + clientNo + " Square of " + clientMessage + " is " + squre;
-//                outStream.writeUTF(serverMessage);
                 outStream.flush();
             }
         }
@@ -57,89 +52,6 @@ public class myThread extends Thread
             System.out.println("Client -" + clientNo + " exit!! ");
         }
     }
-    /*
-    private Thread t;
-    private String tName;
-    String line = null;
-    BufferedReader is = null;
-    PrintWriter os = null;
-    Socket s = null;
-    
-    myThread(String name, Socket s) 
-    {
-        this.tName = name;
-    }
-    
-    //override method
-    public void run()
-    {
-        //implementation of run method
-        System.out.println(this.tName + " is running");
-        try
-        {
-            is = new BufferedReader(new InputStreamReader(s.getInputStream()));
-            os = new PrintWriter(s.getOutputStream());
-
-        }
-        catch (IOException e)
-        {
-            System.out.println("IO error in server thread");
-        }
-
-        try
-        {
-            line = is.readLine();
-            while (line.compareTo("QUIT") != 0)
-            {
-
-                os.println(line);
-                os.flush();
-                System.out.println("Response to Client  :  " + line);
-                line = is.readLine();
-            }
-        }
-        catch (IOException e)
-        {
-
-            line = this.getName(); //reused String line for getting thread name
-            System.out.println("IO Error/ Client " + line + " terminated abruptly");
-        }
-        catch (NullPointerException e)
-        {
-            line = this.getName(); //reused String line for getting thread name
-            System.out.println("Client " + line + " Closed");
-        }
-        finally
-        {
-            try
-            {
-                System.out.println("Connection Closing..");
-                if (is != null)
-                {
-                    is.close();
-                    System.out.println(" Socket Input Stream Closed");
-                }
-
-                if (os != null)
-                {
-                    os.close();
-                    System.out.println("Socket Out Closed");
-                }
-                if (s != null)
-                {
-                    s.close();
-                    System.out.println("Socket Closed");
-                }
-
-            }
-            catch (IOException ie)
-            {
-                System.out.println("Socket Close Error");
-            }
-        }
-    
-    }
-    */
     public void start () 
     {
         System.out.println("Starting ");
